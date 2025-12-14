83731e4e054fb6ecbba7697fcccd746d4c3a3d05
zahiranisa1012-commits <zahiranisa1012@gmail.com>
2025-12-14 10:20:41 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+
+<script>
+const firebaseConfig = {
+    apiKey: "ISI_API_KEY",
+    authDomain: "ISI_AUTH_DOMAIN",
+    projectId: "ISI_PROJECT_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+</script>
